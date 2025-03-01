863ad32ae415047b7c0c268807806ae88c242282
devg93 <gtugushi67@gmail.com>
2025-03-01 19:32:20 +0400

    ddd

--- obj/Debug/net8.0/GithuabAction.AssemblyInfo.cs
+++ obj/Debug/net8.0/GithuabAction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GithuabAction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96cbeadffb1cb2395a22bbfd0b4cb6c6ec28f5d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9094bb43a0ed820974e8b6f4a84239e16ffca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GithuabAction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GithuabAction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
